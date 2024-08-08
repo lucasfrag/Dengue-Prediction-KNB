fc702cddef1e3fbe22c4c84d70434d8160ef7ae7
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-08-08 20:50:56 -0300

    Transformar os dados

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 # Carregar a base de dados
 df = pd.read_csv('arq_dengue.csv', delimiter=';')
-print(df.head())
 
+# Transformer os valores "não/sim" em 0/1 e exibir os primeiros registros
+df = df.replace({'sim':1, 'nao':0})
+print(df.head())
 
 
